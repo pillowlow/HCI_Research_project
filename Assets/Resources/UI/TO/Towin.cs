f3643e1543b9c8da0096dd85c4179a90f740a74c
RyoyakS <bill121789@gmail.com>
2021-06-16 11:34:01 +0800

    All UI 100% task1~3 finished

---
+++ Assets/Resources/UI/TO/Towin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Towin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         Button button = GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("win");
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
